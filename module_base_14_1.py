1b74b6122939a731714377b836521474a43030d8
Anatoliy-Bor <89204220661@mail.ru>
2024-11-26 14:59:46 +0300

    Update module_base_14_1.py

--- module_base_14_1.py
+++ module_base_14_1.py
@@ -15,28 +15,17 @@ balance INTEGER NOT NULL
 cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
 for i in range(10):
-	c = i+1
-	eg = (i+1)*10
-	blnc = 1000
-	cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?) ", (f"newuser{c}", f"example{c}ex@mail.ru", f"{eg}",f"{blnc}"))
+	cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?) ", (f"newuser{i+1}", f"example{i+1}ex@mail.ru", f"{(i+1)*10}", "1000"))
 
-c = 0
-cursor.execute("SELECT * FROM Users")
-users = cursor.fetchall()
-for user in users:
-	c += 1
-	if c % 2 != 0:
-		 cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"newuser{c}"))
-
+for i in range(1, 11,  2):
+		 cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"newuser{i}"))
 
 cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
-
 
 cursor.execute("SELECT * FROM Users WHERE age != ?", (60,))
 users = cursor.fetchall()
 for data in users:
     print(f" Имя: {data[1]} | Почта: {data[2]} | Возраст: {data[3]} | Баланс: {data[4]}")
 		
-
 connection.commit()
 connection.close()
