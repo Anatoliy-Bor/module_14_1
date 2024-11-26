f3bef21974d77bb82e1816ececf5a083051024d7
Anatoliy-Bor <89204220661@mail.ru>
2024-11-26 15:10:48 +0300

    Update module_base_14_1.py

--- module_base_14_1.py
+++ module_base_14_1.py
@@ -17,13 +17,14 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 for i in range(10):
 	cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?) ", (f"newuser{i+1}", f"example{i+1}ex@mail.ru", f"{(i+1)*10}", "1000"))
 
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
+
 for i in range(1, 11,  2):
 		 cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"newuser{i}"))
 
-cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
-
 cursor.execute("SELECT * FROM Users WHERE age != ?", (60,))
 users = cursor.fetchall()
+
 for data in users:
     print(f" Имя: {data[1]} | Почта: {data[2]} | Возраст: {data[3]} | Баланс: {data[4]}")
 		
